6b1c45799670ef8bcefcf2e71a6db5c3d44eedb5
Wahe3bru <wahe3bru@gmail.com>
2019-08-15 15:30:17 +0200

    new post template

---
+++ new_post.py
@@ -0,0 +1,24 @@
+#!/usr/bin/Python
+from datetime import datetime
+from pathlib import Path
+import sys
+
+Path()
+title = sys.argv[1]
+date_string = datetime.now().strftime("%Y-%m-%d")
+filename = date_string + "-" + "-".join(title.lower().split(" ")) + ".md"
+path_file = Path() / '_posts' / filename
+
+path_file.touch()
+path_file.write_text(f'''---
+title: {title}
+last_modified_at: {date_string}T15:17:02-05:00
+categories:
+  - blog
+tags:
+  - python
+  - Jupyter
+  - notes
+  - tutorial
+---
+''')
