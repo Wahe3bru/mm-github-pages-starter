ece3b84b9a680cd7831297ccd41202eb7885be4b
Waheeb Agherdien <wahe3bru@gmail.com>
2019-08-15 21:06:10 +0200

    wide: class and default cat n tags

--- new_post.py
+++ new_post.py
@@ -14,11 +14,14 @@ title: {title}
 last_modified_at: {date_string}T15:17:02-05:00
 categories:
   - blog
+  - thoughts
 tags:
   - python
   - Jupyter
   - notes
   - tutorial
+  - thoughts
+classes: wide
 header:
   image: https://source.unsplash.com/collection/8375052/1024x720
 ---
